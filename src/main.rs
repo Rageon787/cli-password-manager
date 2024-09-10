5ce4d88ae1928147b2175fc309cc9b718f3d9d37
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2024-09-11 09:53:14 +1000

    Defined a function for password generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+fn generate_password () {
+    // generate a password from a set of options 
+    //      alpha, numeric, alpha-numeric 
+    //      special characters (y / n) 
+    //      Capitalization if (alpha / alphanumeric)  
+    //      Length of password  
+} 
+
+fn display_vault() {
+    // display the vault or a sub-vault
+}
+
 fn main() {
     println!("Hello, world!");
 }
